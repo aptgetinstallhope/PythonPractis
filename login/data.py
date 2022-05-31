28f8b0997140b27a9b0cb398c609f7228a9a1c99
aptgetinstallhope <aptgetinstallhope@gmail.com>
2022-05-31 03:37:28 -0400

    login

---
+++ login/data.py
@@ -0,0 +1,5 @@
+users = {
+    '1': {'username':'xiii', 'firstname':'ali', 'lastname':'ghazali', 'password':'123'},
+    '2': {'username':'kz', 'firstname':'hossein', 'lastname':'kazemi', 'password':'1234'},
+    '3': {'username':'most4f', 'firstname':'mostafa', 'lastname':'saatsaz', 'password':'12345'},
+}
