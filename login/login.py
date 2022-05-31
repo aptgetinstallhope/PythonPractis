b0a915b44aa79a8b770d3031b30ade2940554875
aptgetinstallhope <aptgetinstallhope@gmail.com>
2022-05-31 05:20:37 -0400

    signup

--- login/login.py
+++ login/login.py
@@ -1,3 +1,19 @@
+from data import users
+username = input('username ==> ')
+passwrod = input('password ==> ')
 
+status = 500
 
+for keyid in users:
+    userobject = users[keyid]
+    if userobject['username'] == username : 
+        if userobject['password'] == passwrod:
+            status = 200
+            print('login successfully')
+            print('your first name is :' + userobject['firstname'])
+            print('your last name is :' + userobject['lastname'])
+            break
+
+if status == 500:
+    print('your username or password is incorrect')
 
